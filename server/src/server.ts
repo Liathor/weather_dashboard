4f3640b494551be828bf61730a18b56f81a73898
Liathor <dyermisha@gmail.com>
2025-02-22 00:41:19 -0500

    Trying a different different route for index.html round 12

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
 
 // Import the routes
@@ -9,8 +11,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Serve static files of entire client dist folder
-app.use(express.static('../../client/dist'));
+app.use(express.static(path.resolve(__dirname, '../../client/dist')));
 
 // Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
